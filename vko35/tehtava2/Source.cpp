2450e8cc37b8037b6f8081c7b00e84529cab2b92
Jere <Jere@DESKTOP-PR3VV46>
2017-08-30 10:50:06 +0300

    Grade enter function working

--- vko35/tehtava2/Source.cpp
+++ vko35/tehtava2/Source.cpp
@@ -1,1 +1,26 @@
 // vko35 tehtava2
+
+#include <iostream>
+
+using std::cout; using std::cin; using std::endl;
+
+int main ()
+{
+	const int SIZE = 6;
+	int grades[SIZE] = { 0 };
+	int grade = 0;
+	cout << "Please enter grades ranging from 0 to 5, to end program enter -1." << endl;
+	do 	{
+		cout << "Enter grade: ";
+		cin >> grade;
+		if (grade <= 0 && grade >= 5)
+			grades[grade]++;
+		if (grade >= 5)
+			cout << "Grade was out of limits" << endl;
+	
+	} while (grade >= 0);
+
+
+	system("pause");
+	return 0;
+}
