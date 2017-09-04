a5f8529205517f910afeef3d4a7dc5eed4ee52bc
Jere <Jere@DESKTOP-PR3VV46>
2017-09-04 15:53:44 +0300

    Works better.

--- vko35/tehtava3(tai_4)/Source.cpp
+++ vko35/tehtava3(tai_4)/Source.cpp
@@ -12,15 +12,25 @@ int main()
 	int lotto[AMOUNT] = { {0} };
 	int ownNumbers[AMOUNT] = { {0} };
 	int winnings = 0;
+	int check = 0;
+	int test = 0;
 	srand(time(NULL));
 
-
-
 	cout << "Enter seven lottery numbers ranging from 1 to 42: ";
-	for (int i = 0; i < AMOUNT; i++)
-		cin >> ownNumbers[i];
-	
-	cout << "Here are your numbers: ";
+
+		for (int i = 0; i < AMOUNT; i++)
+			do
+			{
+				test = 0;
+				cin >> ownNumbers[i];
+				if (ownNumbers[i] < 1 || ownNumbers[i] > 42)
+				{
+					cout << "Number was not within limits" << endl;
+					test++;
+				}
+
+			} while (test != 0);
+		cout << "Here are your numbers: ";
 
 	for (int i = 0; i < AMOUNT; i++)
 		cout << ownNumbers[i] << " ";
@@ -29,7 +39,17 @@ int main()
 
 	for (int i = 0; i < AMOUNT; i++)
 	{
-		lotto[i] = rand() % 42 + 1;
+		do
+		{
+			check = 0;
+			lotto[i] = rand() % 42 + 1;
+				for (int u = 0; u < AMOUNT; u++)
+					if (i != u)
+					{
+						if (lotto[i] == lotto[u])
+							check++;
+					}
+		} while (check != 0);
 		cout << lotto[i];
 		if (i < 6)
 			cout << ", ";
@@ -47,7 +67,4 @@ int main()
 
 	system("pause"); 
 	return 0;
-
-
-
 }
