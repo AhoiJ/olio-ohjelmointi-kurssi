044a972de20a6a0f9e0e1e4c6b728bc757962f9e
Jere <Jere@DESKTOP-PR3VV46>
2017-09-21 13:58:17 +0300

    Opettaja.cpp aloitettu header ehkä valmis

--- vko38/t3-t44/Opettaja.cpp
+++ vko38/t3-t44/Opettaja.cpp
@@ -2,11 +2,61 @@
 
 
 
-Opettaja::Opettaja()
+Opettaja::Opettaja(): palkka_(0.0) // string - luokka alustaa muut
+{
+}
+
+Opettaja::Opettaja(string tunnus, string enim, string snim, float palkka) :
+	tunnus_(tunnus), enim_(enim), snim_(snim), palkka_(palkka)
 {
 }
 
 
 Opettaja::~Opettaja()
 {
+}
+
+string Opettaja::annaTunnus()
+{
+	return tunnus_;
+}
+
+string Opettaja::annaEnim()
+{
+	return enim_;
+}
+
+string Opettaja::annaSnim()
+{
+	return snim_;
+}
+
+float Opettaja::annaPalkka()
+{
+	return 0.0f;
+}
+
+void Opettaja::asetaTunnus(string tunnus)
+{
+	tunnus_ = tunnus;
+}
+
+void Opettaja::asetaEnim(string enim)
+{
+	enim_ = enim;
+}
+
+void Opettaja::asetaSnim(string snim)
+{
+	snim_ = snim;
+}
+
+void Opettaja::asetaPalkka(float palkka)
+{
+	palkka_ = palkka;
+}
+
+void Opettaja::tulosta()
+{
+	cout << tunnus_ << " " << enim_ << " " << " " << snim_ << " " << palkka_ << endl;
 }
--- vko38/t3-t44/Opettaja.h
+++ vko38/t3-t44/Opettaja.h
@@ -1,8 +1,32 @@
 #pragma once
+#include <iostream>
+using std::string;
+
+
 class Opettaja
 {
 public:
 	Opettaja();
+	Opettaja(string tunnus, string enim, string snim, float palkka);
 	~Opettaja();
+	// getters
+	string annaTunnus();
+	string annaEnim();
+	string annaSnim();
+	float annaPalkka();
+
+	// setters
+	void asetaTunnus(string tunnus);
+	void asetaEnim(string enim);
+	void asetaSnim(string snim);
+	void asetaPalkka(float palkka);
+
+	void tulosta();
+
+	private:
+		string tunnus_;
+		string enim_;
+		string snim_;
+		float palkka_;
 };
 
